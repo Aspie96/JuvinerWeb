25331dfffbb9c9d0988d19b80bc20c0767386489
Valentino Giudice <valentino.giudice96@gmail.com>
2020-07-09 23:12:51 +0200

    Lorem

--- src/main/java/com/mycompany/juvinerweb/api/Api.java
+++ src/main/java/com/mycompany/juvinerweb/api/Api.java
@@ -82,7 +82,7 @@ public class Api {
     public Object getRoot() {
         List<Section> sections = new ArrayList<Section>();
         sectionDao.findAll().forEach(section -> sections.add(section.toSection(true, false, false, false, false, false, false)));
-        Root root = new Root("", sections);
+        Root root = new Root("Lorem ipsum dolor sit amet, consectetur adipisci elit, sed do eiusmod tempor incidunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrum exercitationem ullamco laboriosam, nisi ut aliquid ex ea commodi consequatur. Duis aute irure reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint obcaecat cupiditat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.", sections);
         return new ApiSuccessResponse("root", root);
     }
     
