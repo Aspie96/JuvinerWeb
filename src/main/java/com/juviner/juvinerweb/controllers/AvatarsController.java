36cfe74000535ca8820f8cdb2708e19a9da9c38b
Valentino Giudice <valentino.giudice96@gmail.com>
2021-01-14 07:08:14 +0100

    P

--- src/main/java/com/juviner/juvinerweb/controllers/AvatarsController.java
+++ src/main/java/com/juviner/juvinerweb/controllers/AvatarsController.java
@@ -2,6 +2,7 @@ package com.juviner.juvinerweb.controllers;
 
 import java.io.FileInputStream;
 import java.io.IOException;
+import org.apache.commons.io.IOUtils;
 import org.springframework.core.io.ClassPathResource;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -13,11 +14,11 @@ class AvatarsController {
     @GetMapping("/avatars/{username}/{image}")
     public @ResponseBody byte[] getImage(@PathVariable String username) throws IOException {
         System.out.println(getClass().getResourceAsStream("D:\\Downloads\\avatars\\" + username));
-        return new ClassPathResource("./avatars/" + username).getInputStream().readAllBytes();
+        return IOUtils.toByteArray(new ClassPathResource("./avatars/" + username).getInputStream());
     }
     
     @GetMapping("/default_avatar")
     public @ResponseBody byte[] getImage() throws IOException {
-        return new ClassPathResource("./avatars/default.jpg").getInputStream().readAllBytes();
+        return IOUtils.toByteArray(new ClassPathResource("./avatars/default.jpg").getInputStream());
     }
 }
